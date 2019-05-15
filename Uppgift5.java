6dc4dfec06780bdcc6ac7ca5668b87a20d4f47ea
Robert Savela <strobbann@gmail.com>
2019-05-15 11:00:56 +0200

    Changed linebreak to modulus

--- Uppgift5.java
+++ Uppgift5.java
@@ -1,27 +1,20 @@
 public class Uppgift5 {
 	
 	public static void main(String[] args) {
-		
-		int lineBreak = 0;
 		for(int i = 1 ; i <= 30; i++) {
-			if(lineBreak == 5){
-				System.out.println();
-				lineBreak = 0;
-			}
-			lineBreak++;
 			System.out.print(i + " ");
 		}
 		System.out.println();
 		int i = 1;
-		lineBreak = 0;
-		do{
-			if(lineBreak == 5){
+		
+		while(i <= 30){
+			System.out.print(i + " ");
+			if(i%5 == 0){
 				System.out.println();
-				lineBreak = 0;
 			}
-			lineBreak++;
-			System.out.print(i++ + " ");
-		}while(i <= 30);
+			i++;
+			
+		}
 		
 	}
 
